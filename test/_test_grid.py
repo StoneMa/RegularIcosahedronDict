1658f2b31f54aba825b7f94b31c5aa5a3af52bd2
kanairen <turutekab@gmail.com>
2016-08-18 00:13:49 +0900

    add test for Grid3d::__init__

--- test/_test_grid.py
+++ test/_test_grid.py
@@ -2,11 +2,24 @@
 # coding: utf-8
 
 import unittest
+import numpy as np
+from src._grid import Grid3d
 
 
 class TestGrid3d(unittest.TestCase):
     def setUp(self):
-        pass
+        self.vertices = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+        self.grid_faces = [[0, 1, 2]]
 
     def tearDown(self):
         pass
+
+    def test_init(self):
+        # list
+        Grid3d(self.vertices, self.grid_faces, n_div=1)
+        # numpy
+        Grid3d(np.array(self.vertices), np.array(self.grid_faces), n_div=1)
+
+
+if __name__ == '__main__':
+    unittest.main()
