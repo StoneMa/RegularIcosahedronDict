c45c8c99a23900877b132da16d296c7a83c1ebb2
kanairen <turutekab@gmail.com>
2016-08-18 16:04:41 +0900

    add test for Grid3d::load

--- test/_test_grid.py
+++ test/_test_grid.py
@@ -11,6 +11,10 @@ class TestGrid3d(unittest.TestCase):
         self.vertices = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
         self.grid_faces = [[0, 1, 2]]
 
+        self.grid_path = "../res/new_regular_ico.grd"
+
+        self.is_print_enabled = False
+
     def tearDown(self):
         pass
 
@@ -19,6 +23,12 @@ class TestGrid3d(unittest.TestCase):
         Grid3d(self.vertices, self.grid_faces, n_div=1)
         # numpy
         Grid3d(np.array(self.vertices), np.array(self.grid_faces), n_div=1)
+
+    def test_load(self):
+        grid3d = Grid3d.load(self.grid_path)
+
+        if self.is_print_enabled:
+            print grid3d
 
 
 if __name__ == '__main__':
