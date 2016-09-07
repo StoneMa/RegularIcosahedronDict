44194bf3e8cd28721749765a69a26a977933fe62
kanairen <turutekab@gmail.com>
2016-09-07 15:47:07 +0900

    add コンテナ要素の型チェック関数を追加

--- src/util/debug_util.py
+++ src/util/debug_util.py
@@ -2,3 +2,19 @@
 # coding: utf-8
 
 
+def assert_type_in_container(container, class_or_type_or_tuple):
+    """
+
+    引数に渡したコンテナの各要素に対して型チェックを行う
+
+    :type container: list
+    :param container: 要素を型チェックされるコンテナ
+
+    :type class_or_type_or_tuple: T or tuple(T)
+    :param class_or_type_or_tuple: 型あるいは型のtuple
+
+
+    """
+    assert hasattr(container, "__getitem__")
+    for elem in container:
+        assert isinstance(elem, class_or_type_or_tuple)
