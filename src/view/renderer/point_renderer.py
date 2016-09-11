ad8d44ad6706d9675e96088a7ae56c211eb92674
kanairen <turutekab@gmail.com>
2016-09-11 15:43:40 +0900

    add OpenGL上で点描画を行うクラスPointRendererを追加

--- src/view/renderer/point_renderer.py
+++ src/view/renderer/point_renderer.py
@@ -1,3 +1,31 @@
 #!/usr/bin/env python
 # coding: utf-8
 
+from OpenGL import GL
+from abstract_renderer import AbstractRenderer
+from src.obj3d import Obj3d
+
+
+class PointRenderer(AbstractRenderer):
+    def __init__(self, model):
+        """
+
+        :type model: Obj3d
+        :param model: 描画する３Dオブジェクト
+
+        """
+        self.model = model
+
+    def render(self):
+        """
+
+        OpenGLによる描画を行う
+
+        """
+
+        vertices = self.model.vertices if self.model is not None else []
+
+        GL.glBegin(GL.GL_QUADS)
+        for vx, vy, vz in vertices:
+            GL.glVertex3f(vx, vy, vz)
+        GL.glEnd()
