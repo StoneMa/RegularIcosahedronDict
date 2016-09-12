38b7487ae6798471327d6e68b76269699a29743c
kanairen <turutekab@gmail.com>
2016-09-12 15:32:06 +0900

    fix import

--- src/map/shape_map.py
+++ src/map/shape_map.py
@@ -6,7 +6,7 @@ import struct
 import numpy as np
 from src.obj.obj3d import Obj3d
 from src.obj.grid.base_grid import BaseGrid
-from src.obj.grid.icosahedron_grid import TriangleGrid, TriangleFace
+from src.obj.grid.icosahedron_grid import TriangleFace
 from src.util.debug_util import assert_type_in_container
 
 
